b8875fe2eb7857b179f33fb7c5c0b6f7e19927a2
Arilson de Jesus da Silva <arilson.silva@neogrid.com>
2020-07-30 11:44:37 -0300

    Aplicatico criado, busca endereço da API ViaCEP.

---
+++ BuscaCep/BuscaCep/BuscaCep/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using BuscaCep.Servico.Modelo;
+using BuscaCep.Servico;
+
+namespace BuscaCep
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BOTAO.Clicked += BuscarCep;
+        }
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            string cep = CEP.Text.Trim().Replace("-", "");
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+                    if (end != null)
+                    {
+                        RESULTADO.Text = string.Format("Endereço: {0}, {1}, {2},{3}.", end.logradouro, end.bairro, end.localidade, end.uf);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "O endereço não foi encontrado para o cep informado: " + cep, "OK");
+                    }
+                }
+                catch(Exception e)
+                {
+                    DisplayAlert("Um erro aconteceu, verifique o cep digitado e sua conexão com a internet e tente novamente.", e.Message, "OK");
+                }
+            }
+        }
+        public bool isValidCEP(string cep)
+        {
+            bool valido = true;
+            int NovoCep = 0;
+
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "CEP Inválido", "O CEP deve conter 8 caracteres.", "OK");
+                valido = false;
+            }
+            
+            if(!int.TryParse(cep, out NovoCep))
+            {
+                DisplayAlert("ERRO", "CEP Inválido", "O CEP deve conter apenas números.", "OK");
+                valido = false;
+            }
+            return valido;
+        }
+    }
+}
---
+++ BuscaCep/BuscaCep/BuscaCep/Servico/ViaCEPServico.cs
@@ -0,0 +1,22 @@
+﻿using BuscaCep.Servico.Modelo;
+using System.Net;
+using Newtonsoft.Json;
+namespace BuscaCep.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEnderecoURL);
+            Endereco endereco = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            if (endereco.cep == null) return null;
+
+            return endereco;
+        }
+    }
+}
